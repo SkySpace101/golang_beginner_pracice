45b83449f0d156c6f37cc181f8d6c28bcfb7072d
Shubham Agarwal <shubham.agarwal480@gmail.com>
2023-12-30 19:59:37 +0530

    Adding the Go Practice files

---
+++ helloworld.go
@@ -0,0 +1,21 @@
+// Package Declaration - Every program is a part of a package. here the package is 'main'.
+// /*
+package main
+
+// Importing Packages - Import files included in the fmt package.
+import (
+	"fmt"
+)
+
+// Functions
+func main() {
+	// Statements and Expressions. - here the Println function is used to Output/print text.
+	fmt.Println("Hello World")
+}
+
+// */
+
+// In go any executable code belongs to the main package.
+
+// ' Compact Code - Bad Practice '
+// package main;import("fmt");func main(){fmt.Println("Hello World");}
