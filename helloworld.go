a1fa93502390427db322326e5d434f4cfb545685
Shubham Agarwal <shubham.agarwal480@gmail.com>
2023-12-31 17:30:38 +0530

    working with function loops and maps

--- helloworld.go
+++ helloworld.go
@@ -10,72 +10,33 @@ import (
 func main() {
 	fmt.Println("Hello World")
 
-	// struct definition
-	type record struct {
-		firstName string
-		lastName  string
-		salary    float64
-		id        string
-		role      string
-	}
+	// myslice := []int{31, 34, 45, 243, 324, 2343}
+	// myslice1 := []int{234, 45, 2, 67, 34, 34}
 
-	// instantiation of the struct record type variable
-	var r1 record
+	// myslice2 := append(myslice, myslice1...)
 
-	r1.firstName = "shubham"
-	r1.lastName = "Agarwal"
-	r1.salary = 200000
-	r1.id = "sad23ds"
-	r1.role = "Senior Developer"
+	mymap := map[int]string{}
 
-	fmt.Println(r1)
-	fmt.Println(r1.salary)
-
-	// map
-
-	mymap := map[int]string{1: "game", 2: "sports", 3: "studies"}
+	mymap[1] = "one"
+	mymap[2] = "two"
+	mymap[3] = "three"
+	mymap[4] = "four"
 
 	fmt.Println(mymap)
 
-	for i := 0; i < 3; i++ {
-		fmt.Println(i)
-		// fmt.Println(mymap[i])
+	for key, val := range mymap {
+		fmt.Printf("the key is %d and the value is %s.\n", key, val)
 	}
 
-	// getting input from the user
+	// fmt.Println(myslice)
+	// fmt.Println(myslice2)
 
-	var a, b int
-	fmt.Println("Please enter a number")
-	fmt.Scan(&a, &b)
-	fmt.Println("thank you")
+	// for i := 0; i < len(myslice); i++ {
+	// 	fmt.Println(myslice[i])
+	// }
+	// fmt.Println(len(myslice2))
+	// fmt.Println(cap(myslice2))
 
-	fmt.Println("please enter a number")
-	// fmt.Scanln(&b)
-	fmt.Println(b)
-
-	var c int
-	fmt.Println("please enter a number")
-	fmt.Scanf("%v", &c) // used when the input is required in a particular format specified in this Scanf function.
-	fmt.Println("thanks")
-	fmt.Println(c)
-
-	fmt.Println(a)
-
-	print_function()
-	print_function()
-	print_function()
-
-	fmt.Println(add(31, 32))
-
-}
-
-func print_function() {
-	fmt.Println("What is good?")
-}
-
-func add(a int, b int) (result int) {
-	result = a + b
-	return
 }
 
 // In Go any executable code belongs to the main package.
