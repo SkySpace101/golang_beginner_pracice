2912080a29e756532f9528bb2c224be39b6f22d9
Shubham Agarwal <shubham.agarwal480@gmail.com>
2023-12-31 12:45:49 +0530

    Working with Arrays

--- helloworld.go
+++ helloworld.go
@@ -10,26 +10,55 @@ import (
 func main() {
 	fmt.Println("Hello World")
 
-	// Four basic types of datatype in Go.
-	// 1. Integer
-	// 2. Float
-	// 3. String
-	// 4. Boolean
+	/*
+			// Array declaration and Assignment.
+			var arr = [4]int{1, 2, 3, 4}
 
-	var a int64
-	a = 23
+			fmt.Println(arr)
 
-	fmt.Println(a)
+			// Declaring and Assigning using walrus operator
+			arr1 := [5]string{"a", "b", "c"} // rest unassigned elements carry default value for that datatype.
 
-	// var b int8 -- produce an error if try to assign 200
-	var b uint8
-	b = 200
+			fmt.Println(arr1)
 
-	fmt.Println(b)
+			// arrays with inferred lengths
+			arr2 := [...]int{1, 2, 3, 4, 5, 5, 6}
 
-	var f1 float32
-	var f2 float64
+			fmt.Println(arr2)
 
+			// accessing the elements of an array
+			fmt.Println(arr2[5])
+			fmt.Println(arr2[3])
+
+			// changing the value of an element of an array
+			arr1[0] = "k"
+			arr2[5] = 41
+
+			fmt.Println(arr2)
+
+			// printing an out of bounds element
+			// fmt.Println(arr1[10]) -- Error: Out of bounds
+
+
+
+		arr1 := [5]int{}              //not initialized
+		arr2 := [5]int{1, 2}          //partially initialized
+		arr3 := [5]int{1, 2, 3, 4, 5} //fully initialized
+
+		fmt.Println(arr1)
+		fmt.Println(arr2)
+		fmt.Println(arr3)
+
+	*/
+
+	// Initialize only Specific elements
+	arr1 := [5]int{1: 10, 2: 40}
+
+	fmt.Println(arr1)
+
+	// getting the length of an array
+
+	fmt.Println(len(arr1))
 }
 
 // In Go any executable code belongs to the main package.
